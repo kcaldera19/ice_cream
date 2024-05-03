b307b92d0ccb9692ce2084c9902f7311907d13e2
Katalina Caldera <katalinacaldera19@gmail.com>
2024-05-03 16:01:36 -0500

    ice Cream

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,44 @@
 "use strict"
+console.log("work");
+
+window.onload = function () {
+    let iceCreamForm = document.querySelector("#iceCreamForm");
+
+    iceCreamForm.addEventListener("submit", iceCreamToppingsCal);
+
+}
+
+
+
+function iceCreamToppingsCal(event) {
+
+    event.preventDefault();
+    let iceForm = event.target;
+    // fixs
+    let totalOrder = 1.25 Number(iceForm.numOfScoops.value);
+
+    let toppingsCost = 0;
+    if (iceForm.coneOrCup.value === "cup") {
+        if (iceForm.sprinkles.checked) {
+            toppingsCost += .50;
+
+        }
+        if (iceForm.whippedCream.checked) {
+            toppingsCost += .25;
+
+        }
+        if (iceForm.hotFudge.checked) {
+            toppingsCost += 1.25;
+
+        }
+        if (iceForm.cherry.checked) {
+            toppingsCost += .25
+        }
+    }
+
+    let totalAmount = totalOrder + toppingsCost;
+    console.log(totalAmount);
+}
+
+
+
