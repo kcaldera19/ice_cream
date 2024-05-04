268f4e2bbab521645fdf4d1bc843bac0a7b779b8
Katalina Caldera <katalinacaldera19@gmail.com>
2024-05-03 19:19:54 -0500

    added comments to ice Cream js

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,31 @@
 "use strict"
 console.log("work");
-
+    // the page run only after the entire page is loaded
 window.onload = function () {
+
+    // can name the varibale anthing I called it iceCreamForm
+    // document is the html page, querySelector grabs the page with the id=icecreamForm
     let iceCreamForm = document.querySelector("#iceCreamForm");
 
+
+    // setup the event listener on the selected form.It listens for the "submit" event.
+    // when the event happens calls the IceCreamToppingCal
     iceCreamForm.addEventListener("submit", iceCreamToppingsCal);
 
 }
 
-
+    // added event(an action such as click or submit)in the parameter of the function
 
 function iceCreamToppingsCal(event) {
-
+    // stops the form from being submited 
     event.preventDefault();
+    // retrieves the target element that gets the event which is iceCreamForm
     let iceForm = event.target;
     // fixs
     let totalOrder = 1.25 *Number(iceForm.numOfScoops.value);
 
+
+    // the topping options
     let toppingsCost = 0;
     if (iceForm.coneOrCup.value === "cup") {
         if (iceForm.sprinkles.checked) {
